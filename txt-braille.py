6fa5bd653b94bc9bbfe054ce52304d2de7f39120
Navin Shrinivas <karupal2002@gmail.com>
2021-02-27 16:06:45 +0530

    deleted stt and got braille to work

--- txt-braille.py
+++ txt-braille.py
@@ -1,38 +1,5 @@
-from flask import Flask, render_template ,redirect , request
-import math
-
-
+from flask import Flask , request ,render_template
 app=Flask(__name__)
-@app.route("/home")
-def home():
-    return render_template('home.html')
-
-@app.route("/")
-def index():
-    return redirect('/home')
-
-
-@app.route("/tippoffservice/<int:x>/<int:y>/<typ>")
-def tippoffservice(x,y,typ):
-    hypo=math.sqrt((math.pow(x,2)+math.pow(y,2)))
-    wa_safeval=1/12
-    a_safeval=2/12
-    current_ratio=x/hypo
-    safe_flag=False
-    if(typ=="wa" and current_ratio < wa_safeval):
-        safe_flag=True
-    elif(typ=="a" and current_ratio < a_safeval):
-        safe_flag=True
-    txt=""
-    if(safe_flag==True):
-        txt="True : the slope is safe to climb over"
-    else:
-        txt="False : the slope is not safe to climb over" #adding the False and True so that any other service and strip and l[0] to see status
-        print('\a')
-        print('\a')
-        print('\a')
-    return render_template('tippoffservice.html', value=txt)
-
 
 @app.route("/txt-braille",methods=['GET','POST'])
 # def message(a):  
@@ -50,6 +17,7 @@ def texttobraille():
             l.append(decode+" ")
         return render_template("texttobraille.html", note=l)
     return render_template("texttobraille.html", note="")
+
 
 
 if __name__=="__main__":
