76e94c204a84387c0957bdbdf765ba0bc914ac82
Navin Shrinivas <karupal2002@gmail.com>
2021-02-27 12:18:10 +0530

    Sub-Service:tippoff basic commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template ,redirect
+import math
+
 
 app=Flask(__name__)
 @app.route("/home")
@@ -9,11 +11,26 @@ def home():
 def index():
     return redirect('/home')
 
+
+@app.route("/tippoffservice/<int:x>/<int:y>/<typ>")
+def tippoffservice(x,y,typ):
+    hypo=math.sqrt((math.pow(x,2)+math.pow(y,2)))
+    wa_safeval=1/12
+    a_safeval=2/12
+    current_ratio=x/hypo
+    safe_flag=False
+    if(typ=="wa" and current_ratio < wa_safeval):
+        safe_flag=True
+    elif(typ=="a" and current_ratio < a_safeval):
+        safe_flag=True
+    txt=""
+    if(safe_flag==True):
+        txt="the slope is safe to climb over"
+    else:
+        txt="the slope is not safe to climb over"
+        print('\a')
+    return render_template('tippoffservice.html', value=txt)
+
+
 if __name__=="__main__":
     app.run()
-
-
-
-# @app.route("/tippoffservice/<x>/<y>")
-# def tippoffservice(x,y):
-    
